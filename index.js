810dec04d25dfc0f942bf2a2ff794acf61cc0aef
Christoffer Bo Petersen <cb.petersen@gmail.com>
2017-09-06 22:37:28 +0200

    :chart_with_upwards_trend: Add opbeat tracking

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env babel-node
 
 // @flow
+require('opbeat').start({
+  appId: '230f25efa8',
+  organizationId: '5e39308c355149cb9fc5280d31ede681',
+  secretToken: process.env.NODE_ENV === 'production' ? '408ab3b63915571f7ad03d8e7726a0d90a29cc63' : ''
+})
 
 require('babel-register')({
   ignore: /node_modules\/(?!nathejk-chat)/
